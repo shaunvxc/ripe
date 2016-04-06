fee895bbf67e4aab379b8afda49f2a1cd4e71841
shaunvxc <shaun@yipitdata.com>
2016-04-06 15:54:06 -0400

    remove unneeded imports

--- ripe/application.py
+++ ripe/application.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
-import sys
 import argparse
 import os
-import subprocess
 
 from ripe import VERSION
 
