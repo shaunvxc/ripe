5a36f2433bb3f662a0cd6a0794243e139087b3e3
shaunvxc <shaun@yipitdata.com>
2016-04-07 12:13:00 -0400

    Update to CLI, more useful package arg
    Please enter the commit message for your changes. Lines starting

--- ripe/application.py
+++ ripe/application.py
@@ -7,8 +7,16 @@ import os
 from ripe import VERSION
 
 def run(package):
-    os.system('echo y | pip uninstall {}'.format(package))
-    os.system('pip install .')
+    os.system('echo y | pip uninstall {}'.format(package.split('/')[-1]))
+
+    if package == os.getcwd().split('/')[-1] or package is None:
+        os.system('pip install .')
+    elif os.path.exists(os.getcwd() + '/{}'.format(package)):
+        # for shorthand, ie `ripe app_name -> pip install ./app_name`
+        os.system('pip install ./{}'.format(package))
+    else:
+        # for longhand paths, ie `ripe ../../app_name`
+        os.system('pip install {}'.format(os.path.expanduser(package)))
 
 
 def prepare_parser():
@@ -22,8 +30,8 @@ def main():
     parser = prepare_parser()
     args = parser.parse_args()
 
-    if not os.path.isfile(os.getcwd() + '/setup.py'):
+    if not os.path.isfile(os.getcwd() + '/setup.py') and not os.path.isfile(os.getcwd() + "/{}/setup.py".format(args.package)):
         print ("Error -- ripe should only be used when testing out versions of a local package, and hence NEEDS")
-        print ("to be run from a `pip install .`-able location" )
+        print ("to be run from or pointed towards a  `pip install .`-able location" )
     else:
         run(args.package)
